6304e4b642c7967de7aa27e04374b34c2b428a08
chu888 <chucheng@protonmail.com>
2017-05-03 00:37:46 -0400

    Update media.py

--- media.py
+++ media.py
@@ -6,7 +6,7 @@ class Movie():
         """
         (This is the constructor for the movie class, so I can create instances of movie.
         These movie objects are located in the "entertainment_center.py."
-        You’ve given movies their own custom data structure by defining the movie class and
+        Youâ€™ve given movies their own custom data structure by defining the movie class and
         constructor, and these objects are stored in a list data structure. )
         """
         self.title = movie_title
@@ -14,7 +14,7 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-#The following code is the instance methods.
+#The following code is the instance methods, it opens the URL for the movie trailer.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
